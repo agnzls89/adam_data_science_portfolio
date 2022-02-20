f936f9e7c76758f6fc2107fe62d22ccc1a82a9cf
Adam <75322652+agnzls89@users.noreply.github.com>
2022-02-19 19:00:13 -0600

    Update fizzbuzz.py

--- Python_Projects/fizzbuzz.py
+++ Python_Projects/fizzbuzz.py
@@ -1,1 +1,15 @@
 
+#if a number is divisible by 3 print(fizz)
+#if a number is divisible by 5 print(buzz)
+#if a number is divisible by 3 and 5 print(fizzbuzz)
+
+for i in range(101):
+    if i % 3 != 0 and i % 5 != 0:
+        print(i)
+    elif i % 3 ==0 and i % 5 != 0:
+        print("FIZZ")
+    elif i % 3 != 0 and i % 5 == 0:
+        print("BUZZ")
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FIZZBUZZ")
+    
